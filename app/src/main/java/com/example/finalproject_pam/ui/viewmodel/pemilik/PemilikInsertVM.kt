a655f4c79a7c5c5f61efea1385d818c58172da48
DimasCW <cdimas292@gmail.com>
2025-01-21 13:34:51 +0700

    Membuat PemilikInsertVM.kt

---
+++ app/src/main/java/com/example/finalproject_pam/ui/viewmodel/pemilik/PemilikInsertVM.kt
@@ -0,0 +1,56 @@
+package com.example.finalproject_pam.ui.viewmodel.pemilik
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject_pam.model.Pemilik
+import com.example.finalproject_pam.repository.PemilikRepository
+import kotlinx.coroutines.launch
+
+class PemilikInsertVM (private val pmlk: PemilikRepository): ViewModel() {
+    var uiState by mutableStateOf(PemilikInsertUiState())
+        private set
+
+    fun updateInsertPmlkState(pemilikinsertUiEvent:PemilikInsertUiEvent) {
+        uiState = PemilikInsertUiState(pemilikinsertUiEvent = pemilikinsertUiEvent)
+    }
+
+    suspend fun insertPemilik() {
+        viewModelScope.launch{
+            try {
+                pmlk.insertPemilik(uiState.pemilikinsertUiEvent.toPmlk())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class PemilikInsertUiState(
+    val pemilikinsertUiEvent: PemilikInsertUiEvent = PemilikInsertUiEvent()
+)
+
+data class PemilikInsertUiEvent(
+    val id_pemilik: String="",
+    val nama_pemilik: String="",
+    val kontak_pemilik: String=""
+)
+
+fun PemilikInsertUiEvent.toPmlk(): Pemilik = Pemilik(
+    id_pemilik = id_pemilik,
+    nama_pemilik = nama_pemilik,
+    kontak_pemilik = kontak_pemilik
+
+)
+
+fun Pemilik.toUiStatePmlk(): PemilikInsertUiState = PemilikInsertUiState(
+    pemilikinsertUiEvent = toPemilikInsertUiEvent()
+)
+
+fun Pemilik.toPemilikInsertUiEvent(): PemilikInsertUiEvent = PemilikInsertUiEvent(
+    id_pemilik = id_pemilik,
+    nama_pemilik = nama_pemilik,
+    kontak_pemilik = kontak_pemilik
+)
