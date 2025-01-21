69ab43908dc27d5f60435c863c65bb1c60f927c4
DimasCW <cdimas292@gmail.com>
2025-01-21 13:33:13 +0700

    menambah serializable

--- app/src/main/java/com/example/finalproject_pam/model/Pemilik.kt
+++ app/src/main/java/com/example/finalproject_pam/model/Pemilik.kt
@@ -1,17 +1,20 @@
 package com.example.finalproject_pam.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Pemilik(
     val id_pemilik: String,
     val nama_pemilik: String,
     val kontak_pemilik: String,
 )
-
+@Serializable
 data class AllPemilikResponse(
     val status: Boolean,
     val message: String,
     val data: List<Pemilik>
 )
-
+@Serializable
 data class PemilikDetailResponse(
     val status: Boolean,
     val message: String,
