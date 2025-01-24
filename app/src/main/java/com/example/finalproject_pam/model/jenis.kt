53bec6f1fa6f509aef4f53f6f19881925f4b6257
DimasCW <cdimas292@gmail.com>
2025-01-24 22:32:22 +0700

    membuat model jenis

--- app/src/main/java/com/example/finalproject_pam/model/jenis.kt
+++ app/src/main/java/com/example/finalproject_pam/model/jenis.kt
@@ -1,2 +1,22 @@
 package com.example.finalproject_pam.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Jenis(
+    val id_jenis: String,
+    val nama_jenis: String,
+    val deskripsi_jenis: String,
+)
+@Serializable
+data class AllJenisResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Jenis>
+)
+@Serializable
+data class JenisDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Jenis
+)
