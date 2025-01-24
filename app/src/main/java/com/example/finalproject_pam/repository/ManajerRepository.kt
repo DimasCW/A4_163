2e015f26b0e8401d5f575b006a206389c026ce90
DimasCW <cdimas292@gmail.com>
2025-01-24 22:34:51 +0700

    membuat manajer repository

---
+++ app/src/main/java/com/example/finalproject_pam/repository/ManajerRepository.kt
@@ -0,0 +1,57 @@
+package com.example.finalproject_pam.repository
+
+
+import com.example.finalproject_pam.model.AllJenisResponse
+import com.example.finalproject_pam.model.AllManajerResponse
+import com.example.finalproject_pam.model.AllPemilikResponse
+import com.example.finalproject_pam.model.Jenis
+import com.example.finalproject_pam.model.Manajer
+import com.example.finalproject_pam.service.JenisService
+import com.example.finalproject_pam.service.ManajerService
+import okio.IOException
+
+
+interface ManajerRepository {
+    suspend fun getManajer(): AllJenisResponse
+    suspend fun insertManajer(manajer: Manajer)
+    suspend fun updateManajer(id_manajer: String, manajer: Manajer)
+    suspend fun deleteManajer(id_manajer: String)
+    suspend fun getManajerById(id_manajer: String): Manajer
+}
+
+class NetworkManajerRepository(
+    private val manajerApiService: ManajerService
+) :ManajerRepository {
+
+
+    override suspend fun insertManajer(manajer: Manajer) {
+        manajerApiService.insertManajer(manajer)
+    }
+
+    override suspend fun updateManajer(id_manajer: String, manajer: Manajer) {
+        manajerApiService.updateManajer(id_manajer, manajer)
+    }
+
+    override suspend fun deleteManajer(id_manajer: String) {
+        try {
+            val response = manajerApiService.deleteManajer(id_manajer)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete jenis. HTTP Status code: " +
+                        "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getManajer(): AllManajerResponse =
+        manajerApiService.getAllManajer()
+
+    override suspend fun getManajerById(id_manajer: String): Manajer {
+        return manajerApiService.getManajerById(id_manajer).data
+    }
+
+}
