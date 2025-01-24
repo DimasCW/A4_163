17c9d63711ebbd338fdc0274790522fd3f7d4728
DimasCW <cdimas292@gmail.com>
2025-01-24 22:34:04 +0700

    menyesuaikan pengelola halaman dengan initializer yang dibutuhkan

--- app/src/main/java/com/example/finalproject_pam/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/PenyediaViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.finalproject_pam.application.PemilikApplications
+import com.example.finalproject_pam.application.Applications
+import com.example.finalproject_pam.ui.viewmodel.jenis.JenisDetailVM
+import com.example.finalproject_pam.ui.viewmodel.jenis.JenisHomeVM
+import com.example.finalproject_pam.ui.viewmodel.jenis.JenisInsertVM
+import com.example.finalproject_pam.ui.viewmodel.jenis.JenisUpdateVM
 import com.example.finalproject_pam.ui.viewmodel.pemilik.PemilikDetailVM
 import com.example.finalproject_pam.ui.viewmodel.pemilik.PemilikHomeVM
 import com.example.finalproject_pam.ui.viewmodel.pemilik.PemilikInsertVM
@@ -13,11 +17,24 @@ import com.example.finalproject_pam.ui.viewmodel.pemilik.PemilikUpdateVM
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
-        initializer { PemilikHomeVM(aplikasiPemilik().container.pemilikRepository) }
-        initializer { PemilikInsertVM(aplikasiPemilik().container.pemilikRepository) }
-        initializer { PemilikDetailVM(createSavedStateHandle(),aplikasiPemilik().container.pemilikRepository) }
-        initializer { PemilikUpdateVM(createSavedStateHandle(),aplikasiPemilik().container.pemilikRepository) }
+        initializer { PemilikHomeVM(aplikasi().container.pemilikRepository) }
+        initializer { PemilikInsertVM(aplikasi().container.pemilikRepository) }
+        initializer { PemilikDetailVM(createSavedStateHandle(),aplikasi().container.pemilikRepository) }
+        initializer { PemilikUpdateVM(createSavedStateHandle(),aplikasi().container.pemilikRepository) }
+
+        initializer { JenisHomeVM(aplikasi().container.jenisRepository) }
+        initializer { JenisInsertVM(aplikasi().container.jenisRepository) }
+        initializer { JenisDetailVM(createSavedStateHandle(),aplikasi().container.jenisRepository) }
+        initializer { JenisUpdateVM(createSavedStateHandle(),aplikasi().container.jenisRepository) }
+
+        initializer { ManajerHomeVM(aplikasi().container.manajerRepository) }
+        initializer { ManajerInsertVM(aplikasi().container.manajerRepository) }
+        initializer { ManajerDetailVM(createSavedStateHandle(),aplikasi().container.manajerRepository) }
+        initializer { ManajerUpdateVM(createSavedStateHandle(),aplikasi().container.manajerRepository) }
     }
-    fun CreationExtras.aplikasiPemilik(): PemilikApplications =
-        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as PemilikApplications)
+    fun CreationExtras.aplikasi(): Applications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as Applications)
+
+
+
 }
