58b7cf0f6a512b8a9383ee5cce99a04594f85573
DimasCW <cdimas292@gmail.com>
2025-01-24 22:34:57 +0700

    membuat manajer model

--- app/src/main/java/com/example/finalproject_pam/model/manajer_properti.kt
+++ app/src/main/java/com/example/finalproject_pam/model/manajer_properti.kt
@@ -1,2 +1,22 @@
 package com.example.finalproject_pam.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Manajer(
+    val id_manajer: String,
+    val nama_manajer: String,
+    val kontak_manajer: String,
+)
+@Serializable
+data class AllManajerResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Manajer>
+)
+@Serializable
+data class ManajerDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Pemilik
+)
